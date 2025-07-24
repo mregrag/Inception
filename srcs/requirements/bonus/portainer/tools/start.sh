d8a912a6b8b2401837fe92e922a4dae5e2780ed6
Mohamed Regrag <mregrag@e3r10p8.1337.ma>
2025-07-24 15:47:56 +0100

    update

--- srcs/requirements/bonus/portainer/tools/start.sh
+++ srcs/requirements/bonus/portainer/tools/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ADMIN_PASSWORD=$(cat /run/secrets/portainer_admin_password)
+
+echo "$ADMIN_PASSWORD" > /tmp/portainer_password
+
 exec /usr/local/bin/portainer/portainer \
   --bind 0.0.0.0:9000 \
   --data /data \
